fe1170fb806ce3c7ce2cbed3cf614b3d457c2da1
imad <imaduddinsykra@gmail.com>
2022-12-08 16:38:13 +0700

    Set Icon/Logo

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,6 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+	<link rel="icon" type="image/x-icon" href="{{ asset('/homepage/images/logo/favicon.ico') }}" />
+	{{-- <link id="icon" rel="shortcut icon" href="{{ asset('/homepage/images/logo.png') }}" type="image/x-icon"> --}}
 	<link href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,700' rel='stylesheet' type='text/css'>
 
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
